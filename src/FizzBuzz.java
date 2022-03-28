9e18a9c59c1b6a36ef10bff7ac1681814b2e4369
josusk8 <josusk8@gmail.com>
2022-03-28 11:11:26 +0100

    No ELSE y -50 Líneas

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,9 +7,9 @@ public class FizzBuzz {
     }
 
     public void mostrar() {
-
         for (int i = 0; i < 100; i++) {
             int [] a = new int[100];
+
             a[i] = i+1;
             if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
                 System.out.println("FizzBuzz");
@@ -26,7 +26,6 @@ public class FizzBuzz {
             if (a[i] >0){
                 System.out.println(a[i]);
             }
-
 
 
         }
