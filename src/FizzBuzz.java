d6f8f87a0c873d30ad39cd7a2f753c29edd5d020
josusk8 <josusk8@gmail.com>
2022-03-28 13:33:42 +0100

    Usando Strings

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,37 +1,42 @@
-import static java.lang.Integer.*;
 
 public class FizzBuzz {
 
-    public FizzBuzz() {
 
-    }
+    static void mostrar() {
+        String numeros = "";
+        String mensaje = "";
+        boolean validador;
+        boolean bloqueo;
+        String[] codigos = {"Fizz", "Buzz", "FizzBuzz"};
 
-    public void mostrar() {
         for (int i = 0; i < 100; i++) {
-            int[] imprimir = new int[100];
-
-            imprimir[i] = i + 1;
-            int fizzbuzzCompare = i;
+            validador = true;
+            bloqueo = false;
 
             if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
-                System.out.println("FizzBuzz");
-                fizzbuzzCompare = -2;
-                imprimir[i] = -1;
+                mensaje = codigos[2];
+                numeros += mensaje + "\r\n";
+                validador = false;
+                bloqueo = true;
             }
-            if ((fizzbuzzCompare + 1) % 3 == 0) {
-                System.out.println("Fizz");
-                imprimir[i] = -1;
+            if ((i + 1) % 3 == 0 && bloqueo == false) {
+                mensaje = codigos[0];
+                numeros += mensaje + "\r\n";
+                validador = false;
             }
-            if ((fizzbuzzCompare + 1) % 5 == 0) {
-                System.out.println("Buzz");
-                imprimir[i] = -1;
-            }
-            if (imprimir[i] > 0) {
-                System.out.println(imprimir[i]);
+            if ((i + 1) % 5 == 0 && bloqueo == false) {
+                mensaje = codigos[1];
+                numeros += mensaje + "\r\n";
+                validador = false;
             }
 
+            if (validador == true || bloqueo == true) {
+                numeros += (i + 1) + "\r\n";
+            }
 
         }
+        System.out.println(numeros);
+
     }
 }
 
---
+++ src/FizzBuzzShould.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzShould {
+
+    @Test
+
+    void mostrar0al100(){
+
+        FizzBuzz.mostrar();
+    }
+}
